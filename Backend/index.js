daaac8cca6a1ff92d3819653506f12f90d1bbe8c
PratsN <pratikshanagawade0@gmail.com>
2022-11-14 23:39:00 +0530

    chat-application

---
+++ Backend/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("Socket app started");
+  res.end();
+});
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+
+  socket.on("joinRoom", (room) => {
+    socket.join(room);
+  });
+
+  socket.on("newMessage", ({ newMessage, room }) => {
+    io.in(room).emit("getLatestMessage", newMessage);
+  });
+});
+
+const port = 9000;
+
+app.listen(port, () => {
+  console.log(`App started on port ${port}`);
+});
